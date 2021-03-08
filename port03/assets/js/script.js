7a0d5b8f7606dfab52b36a060cb863ca52885fbf
hyojudev <hyojudev@gmail.com>
2021-03-08 19:42:18 +0900

    add nav animation and home page

--- port03/assets/js/script.js
+++ port03/assets/js/script.js
@@ -10,7 +10,7 @@
 
 //         document.querySelector(".navigation").classList.toggle("nav_toggle");
 //         document.querySelector(".menu_radial").classList.toggle("menu_radial_toggle");
-//         document.querySelector(".menu").classList.toggle("menu_toggle");
+//         document.querySelector(".menu").classList.toggle("nav_menu_toggle");
 //     }
 
 //     // function navLinkToggle() {
@@ -62,3 +62,71 @@
 
 // };
 // navSlider();
+
+// const mainMenuLink = document.querySelector(".main_menu_link");
+
+// navSplit();
+// navHover();
+
+// function navSplit() {
+//     document.querySelectorAll(".main-split").forEach(elem => {
+//         let navSplitText = elem.innerText;
+//         let navSplitWrap = navSplitText.split("").join("</span><span aria-hidden:'true'>");
+//         navSplitWrap = "<span aria-hidden:'true'>" + navSplitWrap + "</span>";
+//         elem.innerHTML = navSplitWrap;
+//         elem.setAttribute("aria-label", navSplitText);
+//     })
+// };
+
+// function navHover() {
+//     mainMenuLink.addEventListener("mouseenter", function () {
+//         if (mainMenuLink === this) {
+//             gsap.to(".main-split span", { duration: 0.4, rotation: 45, stagger: .04 })
+//         }
+//     });
+//     mainMenuLink.addEventListener("mouseout", function () {
+//         gsap.to(".main-split span", { duration: 0.4, rotation: 0, stagger: .04 })
+//     });
+// };
+
+const hBtn = document.querySelector(".hamburger");
+const mainNavLink = document.querySelectorAll(".main-menu__link");
+const signNavLink = document.querySelectorAll(".sign-menu__link");
+
+function navToggle() {
+    document.querySelector(".navigation").classList.toggle("nav-toggle");
+    document.querySelector(".nav-radial").classList.toggle("menu-radial-toggle");
+    document.querySelector(".nav-menu-wrap").classList.toggle("nav-menu-toggle");
+}
+
+function mainLinkToggle() {
+    mainNavLink.forEach((link, index) => {
+        setTimeout(() => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `mainLinkFade 0.4s ease-in-out forwards ${index / 7 + 0.04}s`;
+            }
+        }, 500);
+    });
+};
+
+function signLinkToggle() {
+    signNavLink.forEach((link, index) => {
+        setTimeout(() => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `signLinkFade 0.4s ease-in-out forwards ${index / 10 + 0.04}s`;
+            }
+        }, 1200);
+    });
+};
+
+function hBtnClick() {
+    navToggle();
+    mainLinkToggle();
+    signLinkToggle();
+}
+
+hBtn.addEventListener("click", hBtnClick);
